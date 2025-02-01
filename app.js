a3289b2eb6ecfda0ff27eef712d9cee7fa9db8ba
N-eeraj <neeraj7rajeena@gmail.com>
2025-02-01 23:28:07 +0530

    Added partials

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 const express = require("express")
-require('dotenv').config()
+require("dotenv").config()
 
 const app = express()
 app.set("view engine", "ejs")
-
-app.listen(process.env.PORT)
+app.use(express.static(__dirname + "/public"))
 
 app.get("/", (_req, res) => {
-  res.render("index")
+  const blogs = [
+    {
+      id: 1,
+      title: "Blog 1",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
+      tags: [
+        "tag 1",
+        "tag 3",
+      ],
+    },
+    {
+      id: 2,
+      title: "Blog 2",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
+      tags: [
+        "tag 2",
+        "tag 4",
+        "tag 5",
+      ],
+    },
+    {
+      id: 3,
+      title: "Blog 3",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
+      tags: [
+        "tag 1",
+      ],
+    },
+  ]
+  res.render("index", {
+    blogs,
+  })
 })
 
 app.get("/login", (_req, res) => {
@@ -22,4 +52,5 @@ app.get("/*", (_req, res) => {
   res.render("404")
 })
 
+app.listen(process.env.PORT)
 console.log(`Server running on port ${process.env.PORT}`)

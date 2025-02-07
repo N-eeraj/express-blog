fa2d17a67da6ebbc3dd2198635a5ac53620e16c5
N-eeraj <neeraj7rajeena@gmail.com>
2025-02-07 09:44:33 +0530

    Updated blogs route & added partial for blog list

--- server/routes/blogs.js
+++ server/routes/blogs.js
@@ -1,108 +1,87 @@
 const express = require("express")
+const { URL } = require("url")
 
 const blogsRouter = express.Router()
 
-blogsRouter.get("/all", (_req, res) => {
-  const blogs = [
-    {
-      slug: 1,
-      title: "Blog 1",
-      author: "Blog 1 Author",
-      createdAt: new Date(),
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
-      tags: [
-        "tag 1",
-        "tag 3",
-      ],
+const blogs = [
+  {
+    slug: "1",
+    title: "Blog 1",
+    author: {
+      id: 1,
+      name: "Blog 1 Author",
     },
-    {
-      slug: 2,
-      title: "Blog 2",
-      author: "Blog 2 Author",
-      createdAt: new Date(),
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
-      tags: [
-        "tag 2",
-        "tag 4",
-        "tag 5",
-      ],
+    createdAt: new Date(),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
+    tags: [
+      "tag 1",
+      "tag 3",
+    ],
+  },
+  {
+    slug: "2",
+    title: "Blog 2",
+    author: {
+      id: 2,
+      name: "Blog 2 Author",
     },
-    {
-      slug: 3,
-      title: "Blog 3",
-      author: "Blog 3 Author",
-      createdAt: new Date(),
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
-      tags: [
-        "tag 1",
-      ],
+    createdAt: new Date(),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
+    tags: [
+      "tag 2",
+      "tag 4",
+      "tag 5",
+    ],
+  },
+  {
+    slug: "3",
+    title: "Blog 3",
+    author: {
+      id: 3,
+      name: "Blog 3 Author",
     },
-    {
-      slug: 4,
-      title: "Blog 4",
-      author: "Blog 4 Author",
-      createdAt: new Date(),
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
-      tags: [
-        "tag 1",
-        "tag 4",
-      ],
+    createdAt: new Date(),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
+    tags: [
+      "tag 1",
+    ],
+  },
+  {
+    slug: "4",
+    title: "Blog 4",
+    author: {
+      id: 4,
+      name: "Blog 4 Author",
     },
-  ]
+    createdAt: new Date(),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
+    tags: [
+      "tag 1",
+      "tag 4",
+    ],
+  },
+]
+
+
+blogsRouter.get("/all", (req, res) => {
+  const pathname = new URL(req.originalUrl, `http://${req.headers.host}`).pathname
+  const page = req.query.page ?? 1
   res.render("blog/list/all", {
     blogs,
+    total: 32,
+    pathname,
+    page,
   })
 })
 
-blogsRouter.get("/my-blogs", (_req, res) => {
-  const blogs = [
-    {
-      slug: 1,
-      title: "Blog 1",
-      author: "Blog 1 Author",
-      createdAt: new Date(),
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
-      tags: [
-        "tag 1",
-        "tag 3",
-      ],
-    },
-    {
-      slug: 2,
-      title: "Blog 2",
-      author: "Blog 2 Author",
-      createdAt: new Date(),
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
-      tags: [
-        "tag 2",
-        "tag 4",
-        "tag 5",
-      ],
-    },
-    {
-      slug: 3,
-      title: "Blog 3",
-      author: "Blog 3 Author",
-      createdAt: new Date(),
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
-      tags: [
-        "tag 1",
-      ],
-    },
-    {
-      slug: 4,
-      title: "Blog 4",
-      author: "Blog 4 Author",
-      createdAt: new Date(),
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
-      tags: [
-        "tag 1",
-        "tag 4",
-      ],
-    },
-  ]
+blogsRouter.get("/my-blogs", (req, res) => {
+  const pathname = new URL(req.originalUrl, `http://${req.headers.host}`).pathname
+  const page = req.query.page ?? 1
   res.render("blog/list/user", {
-    blogs,
+    blogs: blogs.filter(({ author }) => author.id === 2),
+    total: 32,
+    pathname,
+    page,
   })
 })
 
@@ -111,20 +90,10 @@ blogsRouter.get("/create", (_req, res) => {
 })
 
 blogsRouter.get("/:slug", (req, res) => {
-  const blog = {
-    slug: 1,
-    title: "Blog 1",
-    author: "Blog 1 Author",
-    createdAt: new Date(),
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta?",
-    tags: [
-      "tag 1",
-      "tag 3",
-    ],
-  }
+  const blog = blogs.find(({ slug }) => slug === req.params.slug)
   
   if (!blog) {
-    return res.status(404).send("Blog not found")
+    return res.status(404).render("404")
   }
   
   res.render("blog/view", blog)
